6f45eb7f6fb91e0583a14d2161ff1309cd142605
keeley1 <keeleygardner.gardner@gmail.com>
2025-03-06 11:19:48 +0000

    Refactoring API call code

--- server.go
+++ server.go
@@ -3,189 +3,44 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"net/http"
 	"net/url"
-	"sync"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/keeley1/novelti-backend/api"
+	"github.com/keeley1/novelti-backend/caching"
+	"github.com/keeley1/novelti-backend/models"
 )
 
-type CacheItem struct {
-	Data      []Book
-	Timestamp time.Time
-}
+func callGoogleBooksAPI(query string, searchType string, detailed bool) ([]models.Book, error) {
 
-var (
-	cache    sync.Map
-	cacheTTL = 10 * time.Minute
-)
-
-type Book struct {
-	Title         string        `json:"title"`
-	Authors       []string      `json:"authors"`
-	PublishedDate string        `json:"publishedDate"`
-	Cover         string        `json:"thumbnail"`
-	ID            string        `json:"id"`
-	Description   template.HTML `json:"description"`
-}
-
-func getFromGoogleBooks(query string, searchType string, detailed bool) ([]Book, error) {
-	var googleBooksAPIURL string
-
-	if searchType == "id" {
-		googleBooksAPIURL = fmt.Sprintf("https://www.googleapis.com/books/v1/volumes/%s", query)
-	} else if searchType == "searchquery" {
-		googleBooksAPIURL = fmt.Sprintf("https://www.googleapis.com/books/v1/volumes?q=%s&maxResults=20", query)
-	} else {
-		encodedQuery := url.QueryEscape(query + " books")
-		googleBooksAPIURL = fmt.Sprintf("https://www.googleapis.com/books/v1/volumes?q=%s&maxResults=20", encodedQuery)
-	}
-
+	var googleBooksAPIURL = api.ConstructAPIURL(query, searchType)
 	fmt.Println("API URL:", googleBooksAPIURL)
 
-	resp, err := http.Get(googleBooksAPIURL)
+	resp, err := api.MakeAPICall(googleBooksAPIURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch data: %v", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	var books []Book
+	volumeData, err := api.DecodeResponse(resp)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 
-	if searchType == "id" {
-		// Parse single volume response
-		var volumeData map[string]interface{}
-		if err := json.NewDecoder(resp.Body).Decode(&volumeData); err != nil {
-			return nil, fmt.Errorf("failed to parse response: %v", err)
-		}
-
-		// Get volume info
-		if volumeInfo, ok := volumeData["volumeInfo"].(map[string]interface{}); ok {
-			// Check for thumbnail
-			var thumbnail string
-			if imageLinks, ok := volumeInfo["imageLinks"].(map[string]interface{}); ok {
-				if _, ok := imageLinks["thumbnail"].(string); ok {
-					bookID := query
-					thumbnail = fmt.Sprintf("https://books.google.com/books/publisher/content/images/frontcover/%s?fife=w600-h800&source=gbs_api", bookID)
-				}
-			}
-			if thumbnail == "" {
-				return nil, fmt.Errorf("no thumbnail available")
-			}
-
-			book := Book{
-				Title: volumeInfo["title"].(string),
-				ID:    query,
-				Cover: thumbnail,
-			}
-
-			if publishedDate, ok := volumeInfo["publishedDate"].(string); ok {
-				book.PublishedDate = publishedDate
-			}
-
-			if authors, ok := volumeInfo["authors"].([]interface{}); ok {
-				for _, author := range authors {
-					book.Authors = append(book.Authors, author.(string))
-				}
-			}
-
-			if detailed {
-				if description, ok := volumeInfo["description"].(string); ok {
-					book.Description = template.HTML(description)
-				}
-			}
-
-			books = append(books, book)
-		}
-	} else {
-		var data map[string]interface{}
-		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			return nil, fmt.Errorf("failed to parse response: %v", err)
-		}
-
-		var seenIDs = make(map[string]bool)
-
-		if items, ok := data["items"].([]interface{}); ok {
-			for _, item := range items {
-				id, ok := item.(map[string]interface{})["id"].(string)
-				if !ok || seenIDs[id] {
-					continue
-				}
-				seenIDs[id] = true
-
-				volumeInfo, ok := item.(map[string]interface{})["volumeInfo"].(map[string]interface{})
-				if !ok {
-					continue
-				}
-
-				var thumbnail string
-				if imageLinks, ok := volumeInfo["imageLinks"].(map[string]interface{}); ok {
-					if _, ok := imageLinks["thumbnail"].(string); ok {
-						bookID := id
-						thumbnail = fmt.Sprintf("https://books.google.com/books/publisher/content/images/frontcover/%s?fife=w400-h600&source=gbs_api", bookID)
-					}
-				}
-
-				if thumbnail == "" {
-					continue
-				}
-
-				book := Book{
-					Title: volumeInfo["title"].(string),
-					ID:    id,
-					Cover: thumbnail,
-				}
-
-				if publishedDate, ok := volumeInfo["publishedDate"].(string); ok {
-					book.PublishedDate = publishedDate
-				} else {
-					continue
-				}
-
-				if authors, ok := volumeInfo["authors"].([]interface{}); ok {
-					for _, author := range authors {
-						book.Authors = append(book.Authors, author.(string))
-					}
-				}
-
-				if detailed {
-					if description, ok := volumeInfo["description"].(string); ok {
-						book.Description = template.HTML(description)
-					}
-				}
-
-				books = append(books, book)
-			}
-		}
+	books, err := api.ExtractBookInfo(volumeData, searchType, query, detailed)
+	if err != nil {
+		fmt.Println("Error: ", err)
 	}
 
 	return books, nil
 }
 
-func getFromCache(key string) ([]Book, bool) {
-	if cacheItem, ok := cache.Load(key); ok {
-		item := cacheItem.(CacheItem)
-		if time.Since(item.Timestamp) < cacheTTL {
-			return item.Data, true
-		}
-		cache.Delete(key)
-	}
-	return nil, false
-}
-
-func saveToCache(key string, books []Book) {
-	cache.Store(key, CacheItem{
-		Data:      books,
-		Timestamp: time.Now(),
-	})
-}
-
 func main() {
 	router := gin.Default()
 
-	// add CORS middleware
+	// CORS middleware
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:3000"}
 	router.Use(cors.New(config))
@@ -199,18 +54,18 @@ func main() {
 	router.GET("/searchbygenre/:subject", func(c *gin.Context) {
 		genre := c.Param("subject")
 
-		if books, found := getFromCache(genre); found {
+		if books, found := caching.GetFromCache(genre); found {
 			c.JSON(200, books)
 			return
 		}
 
-		books, err := getFromGoogleBooks(genre, "subject", false)
+		books, err := callGoogleBooksAPI(genre, "subject", false)
 		if err != nil {
 			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
-		saveToCache(genre, books)
+		caching.SaveToCache(genre, books)
 		c.JSON(200, books)
 	})
 
@@ -218,18 +73,18 @@ func main() {
 		title := c.Param("title")
 		cacheKey := "title:" + title
 
-		if books, found := getFromCache(cacheKey); found {
+		if books, found := caching.GetFromCache(cacheKey); found {
 			c.JSON(200, books)
 			return
 		}
 
-		books, err := getFromGoogleBooks(title, "intitle", false)
+		books, err := callGoogleBooksAPI(title, "intitle", false)
 		if err != nil {
 			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
-		saveToCache(cacheKey, books)
+		caching.SaveToCache(cacheKey, books)
 		c.JSON(200, books)
 	})
 
@@ -238,18 +93,18 @@ func main() {
 
 		encodedQuery := url.QueryEscape(searchQuery)
 
-		if books, found := getFromCache(encodedQuery); found {
+		if books, found := caching.GetFromCache(encodedQuery); found {
 			c.JSON(200, books)
 			return
 		}
 
-		books, err := getFromGoogleBooks(encodedQuery, "searchquery", false)
+		books, err := callGoogleBooksAPI(encodedQuery, "searchquery", false)
 		if err != nil {
 			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
-		saveToCache(encodedQuery, books)
+		caching.SaveToCache(encodedQuery, books)
 		c.JSON(200, books)
 	})
 
@@ -257,12 +112,12 @@ func main() {
 		id := c.Param("id")
 		cacheKey := "id:" + id
 
-		if books, found := getFromCache(cacheKey); found {
+		if books, found := caching.GetFromCache(cacheKey); found {
 			c.JSON(200, books)
 			return
 		}
 
-		books, err := getFromGoogleBooks(id, "id", true)
+		books, err := callGoogleBooksAPI(id, "id", true)
 		if err != nil {
 			c.JSON(500, gin.H{"error": err.Error()})
 			return
@@ -276,11 +131,11 @@ func main() {
 			return
 		}
 
-		saveToCache(cacheKey, books)
+		caching.SaveToCache(cacheKey, books)
 		c.JSON(200, books)
 	})
 
-	// test endpoint
+	// ------ TESTING ENDPOINT ------
 	router.GET("/test/:query", func(c *gin.Context) {
 		query := c.Param("query")
 		encodedQuery := url.QueryEscape(query)
